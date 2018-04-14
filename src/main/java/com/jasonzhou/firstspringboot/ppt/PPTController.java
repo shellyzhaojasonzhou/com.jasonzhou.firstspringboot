89a0105cae3b98556daee57b869f51a19fb2fdb0
Administrator <Administrator@SC-201803311537>
2018-04-14 18:15:18 +0800

    auth:jasonzhou comment:防止get请求请求头过大，更改为post获取文件路径及名称，get下载

--- src/main/java/com/jasonzhou/firstspringboot/ppt/PPTController.java
+++ src/main/java/com/jasonzhou/firstspringboot/ppt/PPTController.java
@@ -9,8 +9,8 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,55 +19,56 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.aspose.slides.Presentation;
 import com.aspose.slides.SaveFormat;
-import com.google.gson.Gson;
 
 @Controller
 @RequestMapping("pptController")
 public class PPTController {
-	
+
 	@RequestMapping("/toScreenshotsView")
 	@ResponseBody
-    public ModelAndView  toScreenshotsView() {
-        return new ModelAndView("html2canvas/screenshots");
-    }
-	
-	@RequestMapping(value="/toDownloadppt",method= RequestMethod.GET)
+	public ModelAndView toScreenshotsView() {
+		return new ModelAndView("html2canvas/screenshots");
+	}
+
+	@RequestMapping(value = "/addImageToPPT", method = RequestMethod.POST)
 	@ResponseBody
-	public void toDownloadppt(@RequestParam("base64List")  String base64List,HttpServletResponse res)
-	{
-		Gson gson = new Gson();
-		ParameterizedTypeReference<List<String>> type = new ParameterizedTypeReference<List<String>>() {
-		};
-		List<String> list = gson.fromJson(base64List, type.getType());
+	public String addImageToPPT(@RequestBody List<String> base64List) {
+		/*
+		 * Gson gson = new Gson(); ParameterizedTypeReference<List<String>> type = new
+		 * ParameterizedTypeReference<List<String>>() { }; List<String> list =
+		 * gson.fromJson(base64List, type.getType());
+		 */
 		Presentation presentation = new Presentation();
-			presentation.save("e:\\pptDemo.ppt", SaveFormat.Ppt);
-			String fileName = "pptDemo.ppt";
-		    res.setHeader("content-type", "application/octet-stream");
-		    res.setContentType("application/octet-stream");
-		    res.setHeader("Content-Disposition", "attachment;filename=" +fileName);
-		    byte[] buff = new byte[1024];
-		    BufferedInputStream bis = null;
-		    OutputStream os = null;
-		    try {
-		      os = res.getOutputStream();
-		      bis = new BufferedInputStream(new FileInputStream(new File("e:\\"+fileName)));
-		      int i = bis.read(buff);
-		      while (i != -1) {
-		        os.write(buff, 0, buff.length);
-		        os.flush();
-		        i = bis.read(buff);
-		      }
-		    } catch (IOException e) {
-		      e.printStackTrace();
-		    } finally {
-		      if (bis != null) {
-		        try {
-		          bis.close();
-		        } catch (IOException e) {
-		          e.printStackTrace();
-		        }
-		      }
-		    }
-		    System.out.println("success");
+		presentation.save("e:\\pptDemo.ppt", SaveFormat.Ppt);
+		return "pptDemo.ppt";
+	}
+
+	@RequestMapping(value = "/toDownloadppt", method = RequestMethod.GET)
+	public void toDownloadppt(@RequestParam("fileName") String fileName, HttpServletResponse res) {
+		res.setContentType("application/octet-stream;charset=UTF-8");
+		res.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+		byte[] buff = new byte[1024];
+		BufferedInputStream bis = null;
+		OutputStream os = null;
+		try {
+			os = res.getOutputStream();
+			bis = new BufferedInputStream(new FileInputStream(new File("e:\\" + fileName)));
+			int i = bis.read(buff);
+			while (i != -1) {
+				os.write(buff, 0, buff.length);
+				os.flush();
+				i = bis.read(buff);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (bis != null) {
+				try {
+					bis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
