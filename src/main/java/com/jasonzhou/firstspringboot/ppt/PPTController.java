8209d30a7aa842168a74de5590f70684814d16f5
jasonzhou <370177274@qq.com>
2018-08-07 23:28:23 +0800

    添加jar及license
    Signed-off-by: jasonzhou <370177274@qq.com>

--- src/main/java/com/jasonzhou/firstspringboot/ppt/PPTController.java
+++ src/main/java/com/jasonzhou/firstspringboot/ppt/PPTController.java
@@ -4,42 +4,90 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.aspose.slides.IAutoShape;
+import com.aspose.slides.IParagraph;
+import com.aspose.slides.IParagraphCollection;
+import com.aspose.slides.IPortionCollection;
+import com.aspose.slides.ISlide;
+import com.aspose.slides.ITextFrame;
+import com.aspose.slides.License;
 import com.aspose.slides.Presentation;
-import com.aspose.slides.SaveFormat;
+import com.aspose.slides.Collections.Generic.IGenericEnumerator;
 
 @Controller
 @RequestMapping("pptController")
 public class PPTController {
 
+	private static InputStream license;
+    //private static InputStream slides;
+
+    /**
+     * 获取license
+     * 
+     * @return
+     */
+    public static boolean getLicense() {
+        boolean result = false;
+        try {
+            license = PPTController.class.getClassLoader().getResourceAsStream("\\license.xml");   // license路径
+            //slides = PPTController.class.getClassLoader().getResourceAsStream("\\test.ppt");   // 原始ppt路径
+            License aposeLic = new License();
+            aposeLic.setLicense(license);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 	@RequestMapping("/toScreenshotsView")
 	@ResponseBody
 	public ModelAndView toScreenshotsView() {
 		return new ModelAndView("html2canvas/screenshots");
 	}
 
-	@RequestMapping(value = "/addImageToPPT", method = RequestMethod.POST)
+	@RequestMapping(value = "/addImageToPPT")
 	@ResponseBody
-	public String addImageToPPT(@RequestBody List<String> base64List) {
+	public String addImageToPPT() {
+		// 验证License
+        if (!getLicense()) {
+            return "license未找到";
+        }
 		/*
 		 * Gson gson = new Gson(); ParameterizedTypeReference<List<String>> type = new
 		 * ParameterizedTypeReference<List<String>>() { }; List<String> list =
 		 * gson.fromJson(base64List, type.getType());
 		 */
-		Presentation presentation = new Presentation();
-		presentation.save("e:\\pptDemo.ppt", SaveFormat.Ppt);
+		Presentation presentation = new Presentation("e:\\1.pptx");
+		ISlide slide = presentation.getSlides().get_Item(0);
+		IAutoShape shape = (IAutoShape) slide.getShapes().get_Item(0);
+		ITextFrame textframe = shape.getTextFrame();
+		IParagraphCollection paras = textframe.getParagraphs();
+		int len = paras.getCount();
+		IPortionCollection portionCollection = paras.get_Item(0).getPortions();
+		int para =portionCollection.getCount();
+		IGenericEnumerator<IParagraph>  gene = paras.iterator();
+		while (gene.hasNext()) {
+			System.err.println(gene.next().getText());
+		}
+		System.err.println("段落有多少"+len);
+		System.err.println("行数有多少"+para);
+		for(int i=0;i<para;i++)
+		{
+			System.err.println(portionCollection.get_Item(i).getText());
+		}
+		//presentation.save("e:\\pptDemo.ppt", SaveFormat.Ppt);
 		return "pptDemo.ppt";
 	}
 
